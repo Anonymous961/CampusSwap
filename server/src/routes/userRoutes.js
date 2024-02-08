5fa88f761ed59263fcdb868a469e159ded8f797f
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-02-09 01:58:53 +0530

    Feat: login, delete and get a user added with (sql, nosql both)

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 const User = require("../models/user.sql");
+const UserMongo = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 //signup user
+function createToken(_id, username) {
+  return jwt.sign({ _id, username }, process.env.SECRET, { expiresIn: "3d" });
+}
+
 router.post("/signup", async (req, res) => {
   const { username, firstname, lastname, password } = req.body;
   if (!username || !password || !firstname || !lastname) {
@@ -28,17 +34,75 @@ router.post("/signup", async (req, res) => {
 
     const user = await User.create({
       username,
-      firstName:firstname,
-      lastName:lastname,
+      firstName: firstname,
+      lastName: lastname,
       password: hash,
     });
-    res.json(user);
+    const userMongo = await UserMongo.create({ username });
+    const token = createToken(user.id, username);
+    res.json({ user, userMongo, token });
   } catch (err) {
     console.error(err.message);
     res.status(400).json({ error: err.message });
   }
 });
 
-//
+//login user
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    if (!username || !password) {
+      throw Error("All fields are required!!");
+    }
+    const user = await User.findOne({ where: { username: username } });
+
+    if (!user) {
+      throw Error("User does not exist");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      throw Error("Password is incorrect!");
+    }
+    const token = createToken(user.id, username);
+    // res.json({ user, userMongo,token });
+    res.json({ username: user.username, token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json(err.message);
+  }
+});
+
+// //delete user
+router.delete("/:username", async(req, res) => {
+  const {username}=req.params;
+  try {
+    const response= await User.destroy({
+      where:{username}
+    })
+    const deleteFromMongo= await UserMongo.findOneAndDelete({username:username})
+    res.json("User deleted successfully");
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json(err.message);
+  }
+});
+
+//get user details
+router.get("/:username",async(req,res)=>{
+  const {username}=req.params;
+  try {
+    const user=await User.findOne({where:{username}});
+    const userMongo= await UserMongo.findOne({username});
+    res.json({user, userMongo})
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json(err.message);
+  }
+});
+
+// //update user details
+// router.patch("/:username",async);
 
 module.exports = router;
