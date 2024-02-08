fd218c00b7b51cc518abdd916561bf56f1a41156
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-02-08 21:30:03 +0530

    Feat: signin added

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,12 +1,44 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const User = require("../models/user.sql");
 
-//controller functions
-const {loginUser,signupUser}=require("../controllers/userController")
-//login
-router.post("/login",loginUser)
+//signup user
+router.post("/signup", async (req, res) => {
+  const { username, firstname, lastname, password } = req.body;
+  if (!username || !password || !firstname || !lastname) {
+    return res.status(400).json("All fields required!");
+  }
+  try {
+    if (!validator.isEmail(username)) {
+      throw Error("Username not valid");
+    }
+    if (!validator.isStrongPassword(password)) {
+      throw Error(
+        "Password not strong enough. Password must contain capital letter, small letter, number and symbol."
+      );
+    }
+    const exists = await User.findOne({ where: { username: username } });
+    if (exists) {
+      throw Error("User already exists");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-//signUp
-router.post("/signup",signupUser);
+    const user = await User.create({
+      username,
+      firstName:firstname,
+      lastName:lastname,
+      password: hash,
+    });
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json({ error: err.message });
+  }
+});
 
-module.exports=router
+//
+
+module.exports = router;
