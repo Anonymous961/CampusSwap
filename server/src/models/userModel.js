ec7dddba390aa98494dc190b37afdf433f3e7b01
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-02-08 21:30:33 +0530

    Mis: potential nosql user model

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,70 +1,14 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: String,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  username: String,
+  itemListId: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+  ],
 });
-
-// static signup method
-userSchema.statics.signup = async function (name, email, password) {
-  // validation
-  if (!name || !email || !password) {
-    throw Error("All fields must be filled");
-  }
-  if (!validator.isEmail(email)) {
-    throw Error("Email not valid");
-  }
-  if (!validator.isStrongPassword(password)) {
-    throw Error(
-      "Password not strong enough. Password must contain capital letter, small letter, number and symbol."
-    );
-  }
-
-  const exists = await this.findOne({ email });
-
-  if (exists) {
-    throw Error("Email already in use");
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
-
-  const user = await this.create({ name, email, password: hash });
-
-  return user;
-};
-
-userSchema.statics.login = async function (email, password) {
-  //validation
-  if (!email || !password) {
-    throw Error("All fields must be filled!");
-  }
-
-  const user = await this.findOne({ email });
-
-  if (!user) {
-    throw Error("not a valid user");
-  }
-  //if the users password matches with the input
-  const match = await bcrypt.compare(password, user.password);
-
-  if (!match) {
-    throw Error("Password is not correct!");
-  }
-
-  return user;
-};
 
 module.exports = mongoose.model("User", userSchema);
