0939e878ea507ffb8b7bed8b89e0de7b5d9cb4ba
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-02-22 02:05:07 +0530

    Fix: storage shifted

--- server/src/storage.js
+++ server/src/storage.js
@@ -2,9 +2,9 @@ const multer=require('multer');
 const path=require('path');
 const {v4:uuidv4}=require("uuid");
 
-const storage=multer.destination({
+const storage=multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,"../public/images");
+        cb(null,"public/images");
     },
     filename:function(req,file,cb){
         cb(null,uuidv4()+'-'+Date.now()+path.extname(file.originalname));
