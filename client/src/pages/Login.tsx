3f8e8a155358e4a380260f6a500abbb9337e16a6
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-03-09 09:59:33 +0530

    API: login and signup api added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
+// import { useLogin } from "../hooks/useLogin";
 const Login = () => {
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
+    // const {login,error,isLoading}=useLogin();
+    const handleSubmit=async(e)=>{
+      e.preventDefault();
+      try {
+        const res=await axios.post("http://localhost:4000/api/user/login",{username:email,password})
+        console.log(res.data);
+      } catch (error) {
+        console.log(error)
+      }
+      // await login(email,password);
+    }
   return (
     <div className="flex justify-center items-center p-10">
       <div className=" flex flex-row border-2  rounded-md w-3/5 shadow-lg">
-        <div className="flex flex-col justify-center p-10 bg-gray-600 w-1/2 poppins-regular rounded-l-lg">
+        <div className="flex flex-col justify-center p-10 bg-slate-800 w-1/2 poppins-regular rounded-l-lg">
           <h1 className="text-4xl text-gray-100">Discover</h1>
           <p className="text-white">
             Sustainable shopping at{" "}
             <span className="underline text-blue-300">CampusSwap</span>
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center p-10 border-2 w-1/2 poppins-regular">
+        <form className="flex flex-col justify-center items-center p-10 border-2 w-1/2 poppins-regular" onSubmit={handleSubmit}>
           <h2 className="m-8 text-4xl ">Login Here</h2>
           <input
             className="border-2 border-gray-500 p-4  my-3 w-11/12"
@@ -30,7 +43,7 @@ const Login = () => {
             onChange={(e)=>setPassword(e.target.value)}
           />
           <input
-            className="border-2 border-gray-400 w-11/12 my-3 mb-4 bg-slate-900 text-white py-3 px-4"
+            className="border-2 border-gray-400 w-11/12 my-3 mb-4 bg-slate-800 text-white py-3 px-4"
             type="submit"
             value="login"
           />
@@ -38,7 +51,7 @@ const Login = () => {
             New user?
             <Link to="/signup" className="underline text-blue-400">Sign up</Link>
           </p>
-        </div>
+        </form>
       </div>
     </div>
   );
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,34 +1,55 @@
 import { Link } from "react-router-dom";
-import {useState} from'react'
+import { useState } from "react";
+import axios from 'axios';
+
 const SignUp = () => {
-    const [name,setName]=useState("");
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    try{
+      const res= await axios.post("http://localhost:4000/api/user/signup",{firstname:fname,lastname:lname,username:email,password});
+      console.log(res.data);
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div className="flex justify-center items-center p-10">
       <div className=" flex flex-row border-2 w-3/5 shadow-lg">
-        <div className="flex flex-col justify-center items-center p-10 border-2 w-1/2 poppins-regular rounded-l-lg">
+        <form className="flex flex-col justify-center items-center p-10 border-2 w-1/2 poppins-regular rounded-l-lg" onSubmit={handleSubmit}>
           <h2 className="m-8 text-4xl ">Sign Up Here</h2>
-          <input
-            className="border-2 border-gray-500 p-4  my-2 w-11/12"
-            type="text"
-            placeholder="Your Name"
-            value={name}
-            onChange={(e)=>setName(e.target.value)}
-          />
+          <div className="flex flex-row w-11/12">
+            <input
+              className="border-2 border-gray-500 p-4  my-2 w-1/2"
+              type="text"
+              placeholder="First Name"
+              value={fname}
+              onChange={(e) => setFname(e.target.value)}
+            />
+            <input
+              className="border-2 border-gray-500 p-4  my-2 w-1/2"
+              type="text"
+              placeholder="Last Name"
+              value={lname}
+              onChange={(e) => setLname(e.target.value)}
+            />
+          </div>
           <input
             className="border-2 border-gray-500 p-4  my-2 w-11/12"
             type="email"
             placeholder="Email"
             value={email}
-            onChange={(e)=>setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="border-2 border-gray-500 p-4  my-2 w-11/12"
             type="password"
             placeholder="Password"
             value={password}
-            onChange={(e)=>setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <input
             className="border-2 border-gray-400 w-11/12 mb-4 bg-slate-900 text-white py-3 px-4"
@@ -41,12 +62,10 @@ const SignUp = () => {
               Login
             </Link>
           </p>
-        </div>
-        <div className="flex flex-col justify-center p-10 bg-gray-600 w-1/2 poppins-regular rounded-r-lg">
+        </form>
+        <div className="flex flex-col justify-center p-10 bg-slate-800 w-1/2 poppins-regular rounded-r-lg">
           <h1 className="text-3xl text-gray-100">Join the club</h1>
-          <p className="text-white">
-            turn your dorm clutter into cash!
-          </p>
+          <p className="text-white">turn your dorm clutter into cash!</p>
         </div>
       </div>
     </div>
