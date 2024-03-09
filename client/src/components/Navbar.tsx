0edcc05746d9a7856a49dedebc873e17089d0a50
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-03-10 00:24:05 +0530

    Logo: logo changed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -10,17 +10,17 @@ const Navbar = () => {
     <div className="flex flex-row justify-between p-2 px-4 mx-8 my-5 border-b-2  border-gray-300 shadow-lg poppins-regular">
       <div className="flex flex-row items-center justify-between gap-10">
         <img
-          src="https://i.postimg.cc/dVrQJR9g/campus-Swap-Png.png"
+          src="https://i.postimg.cc/BvXyZQcc/campus-Swaplogo.png"
           width={"100px"}
           onClick={()=>navigate("/")}
         />
 
-        <ul className="flex flex-row items-center gap-5">
+        <ul className="flex flex-row items-center gap-5 ">
           <Link to="/">
-          <li className="p-2 text-xl">Home</li>
+          <li className="p-2 text-xl hover:underline">Home</li>
           </Link>
-          <li className="p-2 text-xl">Contact</li>
-          <li className="p-2 text-xl">About</li>
+          <li className="p-2 text-xl hover:underline">Contact</li>
+          <li className="p-2 text-xl hover:underline">About</li>
         </ul>
       </div>
       <div className="flex justify-items-end items-center gap-10">
@@ -28,6 +28,7 @@ const Navbar = () => {
         <Link to={User?"/profile":"/login"}>
           <FaUser className="text-4xl" />
         </Link>
+        {/* <Dropdown/> */}
       </div>
     </div>
   );
